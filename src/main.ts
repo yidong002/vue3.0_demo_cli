312362107b24d8fc94f4fa4b1457a1b23f05a028
xuwei <1098404803@qq.com>
2020-12-08 09:38:59 +0800

    feat: vue3.0框架

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import installElementPlus from './plugins/element'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+// installElementPlus(app)
+app.use(store).use(router).use(ElementPlus).mount('#app')
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,54 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-button type="primary" @click="btEvent">el-button</el-button>
+    <el-timeline>
+      <el-timeline-item
+        v-for="item in timeLineData"
+        :key="item.id"
+        :timestamp="item.time"
+      >
+        {{ item.name }}
+      </el-timeline-item>
+    </el-timeline>
   </div>
 </template>
+
+<script>
+import { reactive, ref } from '@vue/reactivity'
+export default {
+  name: 'About',
+  setup() {
+    let timeLineData = reactive([
+      {
+        id: 1,
+        time: '2020-11-01',
+        name: '评审开始',
+      },
+      {
+        id: 1,
+        time: '2020-11-02',
+        name: '评审进行中',
+      },
+      {
+        id: 1,
+        time: '2020-11-03',
+        name: '评审结束',
+      },
+    ])
+    let { btEvent } = aboutEvent()
+    return {
+      btEvent,
+      timeLineData,
+    }
+  },
+}
+function aboutEvent() {
+  function btEvent() {
+    alert('vue3.0,使用element-plus,成功')
+  }
+  return {
+    btEvent,
+  }
+}
+</script>
